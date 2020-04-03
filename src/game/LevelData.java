99befced89e30bcc562a541cff7372713430ee95
maciejdybowski <Lokomotywa123>
2020-04-03 19:30:36 +0200

    test 2

--- src/game/LevelData.java
+++ src/game/LevelData.java
@@ -27,6 +27,7 @@ public class LevelData {
     private Pane gameRoot;
 
     // asdasdsadasdasdasdasdsadsadadasdadsadsad
+    ////ad sadas
 
     Image Tile_1 = new Image("file:src/asset/Maps/Zestaw_1/Tiles/1.png");
     Image Tile_2 = new Image("file:src/asset/Maps/Zestaw_1/Tiles/2.png");
